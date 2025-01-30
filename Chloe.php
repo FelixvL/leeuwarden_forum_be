a5a55ecbdafad15181567b7d89cb1459e499769f
Chloe <temp@cloudflare.gay>
2025-01-30 16:25:01 +0100

    Mock data for leden cards

--- Chloe.php
+++ Chloe.php
@@ -3,25 +3,43 @@
     header('Access-Control-Allow-Methods: GET, POST');
     header("Access-Control-Allow-Headers: X-Requested-With");
 ?>
+
 [
-	{
-		"images" : [
-			{
-				"fotonaam" : "foto1.jpg"
-			},
-			{
-				"fotonaam" : "foto2.jpg"
-			},
-			{
-				"fotonaam" : "foto3.jpg"
-			},
-		],
-		"straat" : "Atrechtlaan 58",
-		"favoriet" : "0",
-		"postcode" : "5627PJ",
-		"plaatsnaam": "Eindhoven"
-	
-	}
+    {
+    "naam":"frits",
+    "foto":"images/placeholder.png",
+	"description":"Random description for frits. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+	"role":"admin",
+	"joined":"2025-01-25"
+    },
+    {
+    "naam":"karel",
+    "foto":"images/placeholder.png",
+	"description":"Random description for karel. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	"role":"moderator",
+	"joined":"2025-01-27"
+    },
+    {
+    "naam":"joris",
+    "foto":"images/placeholder.png",
+	"description":"Random description for joris. jajaja maar",
+	"role":"member",
+	"joined":"2025-01-29"
+    },
+    {
+    "naam":"maaike",
+    "foto":"images/placeholder.png",
+	"description":"Random description for maaike. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. uhhhh yes",
+	"role":"member",
+	"joined":"2025-01-30"
+    },
+    {
+    "naam":"lisanne",
+    "foto":"images/placeholder.png",
+	"description":"Random description for lisanne. Lorem ipsum dolor sit amet consectetur.",
+	"role":"member",
+	"joined":"2025-01-29"
+    }
 ]
 
 
