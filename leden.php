833c512aae0903fc8e91fedb1044fb6612cd410b
felixvl <digitaleservice@gmail.com>
2025-01-30 12:44:27 +0100

    leden met headers

--- leden.php
+++ leden.php
@@ -1,11 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-
-header('Access-Control-Allow-Methods: GET, POST');
-
-header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
 ?>
-
 [
     {
     "naam":"frits"
