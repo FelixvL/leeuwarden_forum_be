09d31777e1688d89ff1156364cc882347ee3fb81
Chloe <temp@cloudflare.gay>
2025-01-30 14:46:20 +0100

    Chloe.php 👍

---
+++ Chloe.php
@@ -0,0 +1,27 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+?>
+[
+	{
+		"images" : [
+			{
+				"fotonaam" : "foto1.jpg"
+			},
+			{
+				"fotonaam" : "foto2.jpg"
+			},
+			{
+				"fotonaam" : "foto3.jpg"
+			},
+		],
+		"straat" : "Atrechtlaan 58",
+		"favoriet" : "0",
+		"postcode" : "5627PJ",
+		"plaatsnaam": "Eindhoven"
+	
+	}
+]
+
+
