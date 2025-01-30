d9af2b411d2e58526b659e67a0ec3d7c1c77046a
“RunaLeagit config --[200~global user.email “[valid-email]”git config --global user.name “RunaLea <runalea@outlook.com>
2025-01-30 14:40:33 +0100

    woningen runa veranderd

--- woningenRuna.php
+++ woningenRuna.php
@@ -1,6 +1,11 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+?>
 [
     {"Atrechtlaan 85":
-        {"img":<plaatjelink>},
+        {"img":"plaatjelink"},
         {"postcode":"5627PJ"},
         {"plaats":"Eindhoven"},
         {"prijs":349.000},
