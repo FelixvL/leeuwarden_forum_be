ed8cbd3d3720af2585bcff5979e51782d9a56e2d
Figuur <lmulder_89@hotmail.com>
2025-01-30 14:24:27 +0100

    woning toegevoegd aan leden.php

--- leden.php
+++ leden.php
@@ -23,3 +23,23 @@ header("Access-Control-Allow-Headers: X-Requested-With");
     "naam":"lisanne"
     }
 ]
+
+[
+    {
+        "image": [
+            "woning.jpg",
+            "woning2.jpg",
+            "woning3"
+        ],
+        "isNieuw": true,
+        "straat": "Atrechtlaan 58",
+        "postcode": "5627PJ",
+        "plaatsnaam": "Eindhoven",
+        "tedureprijs": "€ 349.000 k.k.",
+        "oppervlakte": "112 m²",
+        "totaleoppervlakte": "178 m²",
+        "slaapkamers": 4,
+        "energielabel": "D",
+        "makelaar": "Woonhub Makelaar"
+    }
+]
