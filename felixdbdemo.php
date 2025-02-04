c69c42f30d09a9846c57c6c8d906f59a232da617
felixvl <digitaleservice@gmail.com>
2025-02-04 15:42:23 +0100

    doen

--- felixdbdemo.php
+++ felixdbdemo.php
@@ -1,8 +1,8 @@
 <?php
 // regels van php
-    echo "dit wil ik zien";
-    $var1 = 3;
-    echo $var1;
+ //   echo "dit wil ik zien";
+   // $var1 = 3;
+   // echo $var1;
 
 $servername = "localhost";
 $username = "root";
@@ -20,10 +20,15 @@ $sql = "SELECT * FROM boot";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    $eindstring = "";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["naamkapitein"]. " " . $row["bootnaam"]. "<br>";
+    $eindstring .= "{";
+    $eindstring .= " \"naamkapitein\" : \"" . $row["naamkapitein"]. "\"";
+    $eindstring .= "},";
   }
+  $eindstring = substr($eindstring, 0, -1);
+  echo $eindstring;
 } else {
   echo "0 results";
 }
