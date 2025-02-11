5d8db4b2b27adfe94551a204258e04de31af21fb
felixvl <digitaleservice@gmail.com>
2025-02-11 17:28:50 +0100

    doen

--- felixonderwerpinsert.php
+++ felixonderwerpinsert.php
@@ -1,4 +1,7 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
   // 1. We halen gegevens op uit de URL-parameters
   $naam = $_GET["naam"];
   $indiener = $_GET["indiener"];
